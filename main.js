a46e240d1ee00a457e02d950869e4b66d98f44e5
mahamad017 <81672479+mahamad017@users.noreply.github.com>
2024-01-24 02:45:24 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let create = document.getElementById("create");
 let login= document.getElementById("login")
 create.addEventListener("click",()=>{
-    window.open("index1.html");
+    window.open("index1.html","_self");
 })
 login.addEventListener("click",()=>{
-    window.open("Table.html");
+    window.open("Table.html","_self");
 })
