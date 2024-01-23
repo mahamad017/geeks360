2cc8b78bb7ee6208ae3c439452a8fe310aeb955e
mahamad017 <khermahamad7@gmail.com>
2024-01-24 01:07:47 +0300

    first edit

---
+++ main.js
@@ -0,0 +1,8 @@
+let create = document.getElementById("create");
+let login= document.getElementById("login")
+create.addEventListener("click",()=>{
+    window.open("index1.html");
+})
+login.addEventListener("click",()=>{
+    window.open("Table.html");
+})
